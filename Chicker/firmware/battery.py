1f8f4b585d96960a7df20cdc473cf2588e289612
Henry Bryant <34583782+HenryR-B@users.noreply.github.com>
2024-02-19 13:04:36 -0800

    update to firmware

--- Chicker/firmware/battery.py
+++ Chicker/firmware/battery.py
@@ -4,7 +4,7 @@ from machine import Pin, ADC
 BATT_LEVEL = ADC(Pin(26)) #channel 0
 ref_5v = ADC(Pin(27)) # channel 1
 ref_3v3 = ADC(Pin(28)) # channel 2
-def Voltages(charge):
+def Voltages(charge_ok):
     BATT_LEVEL_val = BATT_LEVEL.read_u16() # read value, 0-65535 across voltage range 0.0v - 3.3v
     logic_3v3_ref_val = ref_3v3.read_u16()
     ref_5V_val = ref_5v.read_u16()
@@ -24,28 +24,28 @@ def Voltages(charge):
         
     if (battery_voltage >  1.0) and (battery_voltage <= 12.8):
         print("Batteries are Exceptionally low, damage is likely!!")
-        charge = 0
+        charge_ok = 0
     elif (battery_voltage >  12.8) and (battery_voltage <= 13.3):
         print("12.8V to 13.3V GUYS! Batteries are VERY Low! Replace them now!!!!")
-        charge = 0
+        charge_ok = 0
     elif (battery_voltage >  13.3) and (battery_voltage <= 13.8):
         print("13.3V to 13.8V Battery voltage is low, turning off HV charging, batteries need to be replaced!")
-        charge = 0
+        charge_ok = 0
     elif (battery_voltage > 13.8) and (battery_voltage <= 14.7):
         print("13.8V to 14.7V Batteries are below nominal, should replace soon")
-        charge = 1
+        charge_ok = 1
     elif (battery_voltage > 14.7) and (battery_voltage <= 14.9):
         print("14.7V to 14.9V Nominal Battery Voltage")
-        charge = 1
+        charge_ok = 1
     elif (battery_voltage > 14.9) and (battery_voltage <= 16.4):
         print("Working Battery Voltage")
-        charge = 1
+        charge_ok = 1
     elif (battery_voltage > 16.4) and (battery_voltage <= 16.9):
         print("Fully charged  Batteries (or close to it)")
-        charge = 1
+        charge_ok = 1
     else :
         print("Batteries are unplugged")
-        charge = 0
+        charge_ok = 1
     print("3.3V Reference: ", logic_voltage, "V")
     print("5V Reference: ", net_5V, "V")
     
--- Chicker/firmware/main.py
+++ Chicker/firmware/main.py
@@ -7,12 +7,12 @@ from battery import Voltages
 
 
 DONE = Pin(4, Pin.IN)
-SHELL_OFF = Pin(9, Pin.IN)
-BREAKBEAM = Pin(23, Pin.IN)
+#SHELL_OFF = Pin(9, Pin.IN)
+#BREAKBEAM = Pin(23, Pin.IN)
 
-#CHARGE = Pin(5, Pin.OUT)
+CHARGE = Pin(5, Pin.OUT)
 #CHIP = Pin(3, Pin.OUT)
-#KICK = Pin(2, Pin.OUT)
+KICK = Pin(2, Pin.OUT)
 #NOT_DISCHARGE = Pin(8, Pin.OUT)
 
 CAN_LED = Pin(6, Pin.OUT)
@@ -20,22 +20,63 @@ INT_LED = Pin(20, Pin.OUT)
 prev_time_int = 0
 prev_time_can = 0
 prev_time_volt = 0
+prev_time_start_chg = 0
+prev_sim_time = 0
+charge_ok = 0
+startup = 0
+charge_toggle_wait = 0
+done_sim = 0
 
 while True:
+    #Read Inputs:
     
     current_time = utime.ticks_ms()
-
+    
+    #simulation values
+    
+    # if (rising edge of charge ?):
+        # set done_sim = 1
+        # prev_sim_time = current_time
+    # if (current_time - prev_sim_time > 1700)
+        #done_sim = 0
+            
+    done_state = done_sim #DONE.value()
+    # do this only on startup
+    if (current_time <= 2000 and startup == 0)
+        Voltages(charge_ok)
+        
+        if (charge_ok == 1 and done_state == 0)
+            charge = 0
+            # set wait to toggle charge pin
+            charge_toggle_wait = 1
+        # finished startup routine, this will only happen again on reset or boot
+        startup = 1
+        
+    # toggle LEDs at different intervals
     if(current_time - prev_time_can > 400):
         CAN_LED.toggle()
-        prev_time_can = utime.ticks_ms()
+        prev_time_can = current_time
     if(current_time - prev_time_int > 800):
         INT_LED.toggle()
-        prev_time_int = utime.ticks_ms()    
+        prev_time_int = current_time
+        
+    # check voltages every 2 seconds
     if(current_time - prev_time_volt > 2000):
         Voltages(charge)
-        prev_time_volt = utime.ticks_ms()
-    if (charge == 1 and done == 0)
-        # set charge pin
+        prev_time_volt = current_time
+    # done will be 0 if either hasnt started charging, or has finished charging
+    if (charge_ok == 1 and done_state == 0): 
+        # setup charge toggle wait time to set charge pin high
+        if (charge_toggle_wait == 1):  
+            if(current_time - prev_time_start_chg > 50)
+                    charge = 1
+                    charge_toggle_wait = 0
+                    prev_time_chg_wait = current_time
+        # wait for charge cycle to do the charge toggle every 15 seconds
+        elif (current_time - prev_time_chg_wait >= 14950)
+            charge = 0
+            charge_toggle_wait = 1
+            # set charge pin
         
        
         
