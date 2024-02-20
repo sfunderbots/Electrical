9ecd021fbf7380006707e114eba6cb6dc2835576
Henry Bryant <34583782+HenryR-B@users.noreply.github.com>
2024-02-20 00:27:32 -0800

    firmware update

--- Chicker/firmware/battery.py
+++ Chicker/firmware/battery.py
@@ -13,39 +13,41 @@ def Voltages(charge_ok):
     net_5V = round(ref_5V_val *3.3 / 65535.0 * 1.00614 * (5.1+3.0)/3.0, 2) # this is incorrect need to fix later, put the resistor divider in backwards
 
 
-    if (net_5V > 4.45) and (net_5V <= 4.70 ):
-        print("4.6V USB Voltage")
-    elif (net_5V > 4.70) and (net_5V <= 4.90):
-        print("4.8V Battery Voltage (USB could still be plugged in, check LED)")
-    elif (net_5V > 4.90) and (net_5V <= 5.20):
-        print("WARNING!!!!: Series diodes for voltage switch has failed, check circuit!")
-    else :
-        print("uh, guys, somethings really broken!!!! Seriously!")
+    #if (net_5V > 4.45) and (net_5V <= 4.70 ):
+        #print("4.6V USB Voltage")
+    #elif (net_5V > 4.70) and (net_5V <= 4.90):
+        #print("4.8V Battery Voltage (USB could still be plugged in, check LED)")
+    #elif (net_5V > 4.90) and (net_5V <= 5.20):
+        #print("WARNING!!!!: Series diodes for voltage switch has failed, check circuit!")
+    #else :
+        #print("uh, guys, somethings really broken!!!! Seriously!")
         
     if (battery_voltage >  1.0) and (battery_voltage <= 12.8):
-        print("Batteries are Exceptionally low, damage is likely!!")
+        #print("Batteries are Exceptionally low, damage is likely!!")
         charge_ok = 0
     elif (battery_voltage >  12.8) and (battery_voltage <= 13.3):
-        print("12.8V to 13.3V GUYS! Batteries are VERY Low! Replace them now!!!!")
+        #print("12.8V to 13.3V GUYS! Batteries are VERY Low! Replace them now!!!!")
         charge_ok = 0
     elif (battery_voltage >  13.3) and (battery_voltage <= 13.8):
-        print("13.3V to 13.8V Battery voltage is low, turning off HV charging, batteries need to be replaced!")
+        #print("13.3V to 13.8V Battery voltage is low, turning off HV charging, batteries need to be replaced!")
         charge_ok = 0
     elif (battery_voltage > 13.8) and (battery_voltage <= 14.7):
-        print("13.8V to 14.7V Batteries are below nominal, should replace soon")
+        #print("13.8V to 14.7V Batteries are below nominal, should replace soon")
         charge_ok = 1
     elif (battery_voltage > 14.7) and (battery_voltage <= 14.9):
-        print("14.7V to 14.9V Nominal Battery Voltage")
+        #print("14.7V to 14.9V Nominal Battery Voltage")
         charge_ok = 1
     elif (battery_voltage > 14.9) and (battery_voltage <= 16.4):
-        print("Working Battery Voltage")
+        #print("Working Battery Voltage")
         charge_ok = 1
     elif (battery_voltage > 16.4) and (battery_voltage <= 16.9):
-        print("Fully charged  Batteries (or close to it)")
+        #print("Fully charged  Batteries (or close to it)")
         charge_ok = 1
     else :
-        print("Batteries are unplugged")
+        #print("Batteries are unplugged")
         charge_ok = 1
-    print("3.3V Reference: ", logic_voltage, "V")
-    print("5V Reference: ", net_5V, "V")
+    #print("3.3V Reference: ", logic_voltage, "V")
+    #print("5V Reference: ", net_5V, "V")
+    
+    return charge_ok
     
--- Chicker/firmware/main.py
+++ Chicker/firmware/main.py
@@ -30,19 +30,26 @@ done_sim = 0
 charge = 0
 charge_disable = 0
 charge_started = 0
+stop_sim_loop = 0
 while True:
     #Read Inputs:
     
     current_time = utime.ticks_ms()
-    
-    #simulation values
-    
-    # if (rising edge of charge ?):
-        # set done_sim = 1
-        # prev_sim_time = current_time
-    # if (current_time - prev_sim_time > 1700)
-        #done_sim = 0
-            
+    ###############################################
+    #simulation values (correct ones)
+    if (charge == 1 and stop_sim_loop == 0):
+        stop_sim_loop = 1
+        done_sim = 1
+        prev_sim_time = current_time
+        #print(current_time/1000)
+        #print("done signal set to 1")
+    elif (stop_sim_loop == 1 and current_time - prev_sim_time > 1700):
+        done_sim = 0
+        #print(current_time/1000)
+        #print("done signal set to 0")
+    ###############################################
+        
+        
     done_state = done_sim #DONE.value()
     # do this only on startup
     if (startup == 0):
@@ -52,7 +59,9 @@ while True:
             charge = 0
             # set wait to toggle charge pin
             charge_toggle_wait = 1
+            #print(current_time/1000, done_state)
             print("charge toggle on startup")
+            
         # finished startup routine, this will only happen again on reset or boot
         startup = 1
         
@@ -60,6 +69,7 @@ while True:
     if(current_time - prev_time_can > 400):
         CAN_LED.toggle()
         prev_time_can = current_time
+        #print(done_state)
     if(current_time - prev_time_int > 800):
         INT_LED.toggle()
         prev_time_int = current_time
@@ -68,49 +78,60 @@ while True:
     if(current_time - prev_time_volt > 2000):
         charge_ok = Voltages(charge)
         prev_time_volt = current_time
+        
         #print("CHARGE OK:", charge_ok)
         #print("Charge toggle wait", charge_toggle_wait)
         
     
-
-    # done will be 0 if either hasnt started charging, or has finished charging
-    if (charge_ok == 1 and done_state == 0 and charge_disable == 0):
-        # setup charge toggle wait time to set charge pin high
-        if (charge_toggle_wait == 1):  
-            if(current_time - prev_time_start_chg > 50):
-                charge = 1
-                charge_toggle_wait = 0
-                prev_time_chg_wait = current_time
-                print("charge toggle complete, waiting for 15s")
-        # wait for charge cycle to do the charge toggle every 15 seconds
-        elif (current_time - prev_time_chg_wait >= 14950):
-            charge = 0
-            charge_toggle_wait = 1
-            print("charge toggle")
-            # set charge pin
-        # check after 5 seconds if the done signal actually goes low after being high for charging
-        elif (current_time - prev_time_chg_wait > 5000 and charge_started == 1):
-            charge_started = 0 # reset charge_started to zero for the next charge cycle
-            if(done_state == 0):
-                #good
-                charge_disable = 0
-            else :
-                # not good, done does not go low, implying not charging properly
-                charge_disable = 1
-                print("charging disabled, done does not go low: TIMEOUT 5s, not charging properly")
+    if (charge_ok == 1 and charge_disable == 0):
+        
+        # done will be 0 if either hasnt started charging, or has finished charging
+        if (done_state == 0):
+            # setup charge toggle wait time to set charge pin high
+            if (charge_toggle_wait == 1):
+                if (current_time - prev_time_start_chg > 50): 
+                    charge = 1
+                    charge_toggle_wait = 0
+                    prev_time_chg_wait = current_time
+                    #print(current_time/1000, done_state)
+                    print("charge toggle complete, waiting for 15s")
+            # wait for charge cycle to do the charge toggle every 15 seconds
+            elif (current_time - prev_time_chg_wait >= 14950):
+                charge = 0
+                charge_toggle_wait = 1
+                stop_sim_loop = 0
+                #print(current_time/1000, done_state)
+                print("charge toggle (15s waited)")
+                # set charge pin
+            # check after 5 seconds if the done signal actually goes low after being high for charging
+            elif (current_time - prev_time_chg_wait > 5000 and charge_started == 1):
+                charge_started = 0 # reset charge_started to zero for the next charge cycle
+                if(done_state == 0):
+                    #good
+                    charge_disable = 0
+                    print("DONE is low after 5s, assuming normal operation")
+                else :
+                    # not good, done does not go low, implying not charging properly
+                    charge_disable = 1
+                    #print(current_time/1000, done_state)
+                    print("charging disabled, done does not go low: TIMEOUT 5s, not charging properly")
         # check after 50ms if done actually toggles high, implying good flyback chip
         elif (current_time - prev_time_chg_wait > 50 and charge_started == 0):
             if(done_state == 1):
                 # good
                 charge_started = 1 # variable for the 5s timeout check
                 charge_disable = 0
+                #print(current_time/1000, done_state)
+                print("charge started (DONE toggles high properly)")
             else :
                 # not good, disable charging
                 charge_started = 0
                 charge_disable = 1
+                #print(current_time/1000, done_state)
                 print("charging was disabled, no high DONE signal received: no charge cycle started")
-    #else :
+    else :
      #   print("Charging Disabled")
+         charge = 0
         # not good, charging was disabled.
         # need to add checks on how to enable it after this point
         
